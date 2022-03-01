c3cfa5a0c47e0ca47a2d8511a4e765c757021919
JosPerez <jos.perez.mtz@gmail.com>
2022-03-01 15:52:57 -0600

    Manejador de servicios
    - Se crea la fachada base para el servicio
    - Se crea manejador de servicios

--- BackServices/BackServices/WebServices/Base/BSBaseFacade.swift
+++ BackServices/BackServices/WebServices/Base/BSBaseFacade.swift
@@ -5,4 +5,59 @@
 //  Created by jose perez on 01/03/22.
 //
 import Foundation
-final class BSBaseFacade {}
+import UIKit
+/// Delegado de recibir la información.
+public protocol BSResponseDelegate {
+    /// Recibe información correcta
+    ///  - Parameters:
+    ///   - entity: Data de servicio.
+    ///   - requestName: Nombre del servicio
+    func recievedEntity<T>(entity: T, requestName: String)
+}
+open class BSBaseFacade {
+    /// URL base de servicio
+    public var url: String!
+    /// Connection
+    public var connection: BSURLSession
+    /// Delegado de la respuesta.
+    public var delegate: BSResponseDelegate?
+    /// Inicializador
+    public init(url: String) {
+        self.url = url
+        self.connection = BSURLSession()
+    }
+    /// Recibe el  error del servicio
+    ///  - Parameters:
+    ///    - error: Error del servicio.
+    ///    - code: Código der error del servicio.
+    ///    - requestName: Nombre de la petición.
+    open func recievedError(error: Error ,code: Int?, requestName: String) {
+        let baseError = BSErrorBase(message: "Se encontro este error", code: code)
+        self.delegate?.recievedEntity(entity: baseError, requestName: requestName)
+    }
+    /// Decode de la entidad de forma segura.
+    ///  - Parameters:
+    ///    - responseType: Tipo Entidad.
+    ///    - data: data  del servicio.
+    ///    - requestName: Nombre de la petición.
+    public func decodeEntity<T: Codable>(responseType: T.Type, data: Data, requestName: String) {
+        do {
+            let response = try JSONDecoder().decode(responseType, from: data)
+            self.delegate?.recievedEntity(entity: response, requestName: requestName)
+        } catch {
+            let baseError = BSErrorBase(message: "Problema en el decodeo", code: 999)
+            self.delegate?.recievedEntity(entity: baseError, requestName: requestName)
+        }
+    }
+}
+final public class BSErrorBase {
+    /// Mensaje de error
+    var message: String?
+    /// Codigo de error
+    var code: Int?
+    /// Inicializador con parametros
+    public init(message: String?,code: Int?) {
+        self.message = message
+        self.code = code
+    }
+}
---
+++ BackServices/BackServices/WebServices/Base/ManagerSession/BSURLSession.swift
@@ -0,0 +1,76 @@
+//
+//  BSURLSession.swift
+//  BackServices
+//
+//  Created by jose perez on 01/03/22.
+//
+import Foundation
+/// Delegado de recibir la información.
+public protocol BSConnectionDelegate {
+    /// Recibe información correcta
+    ///  - Parameters:
+    ///   - data: Data de servicio
+    ///   - requestName: Nombre del servicio
+    func recievedData(data: Data, requestName: String)
+    /// Recibe el error de servicio
+    ///  - Parameters:
+    ///   - Error: Error del servicio
+    ///   - code: Error HTTP
+    ///   - requestName: Nombre del servicio
+    func recievedError(error: Error ,code: Int?, requestName: String)
+}
+/// Enumerador encargado revisar respuesta de servicio
+enum BSResponeCode {
+    /// Caso respuesta Exitosa,
+    case success
+    /// Caso respuesta Fallida con código,
+    case failed(code: Int)
+    public init(rawValue: Int) {
+        if rawValue == 200 {
+            self = .success
+        } else {
+            self = .failed(code: rawValue)
+        }
+    }
+}
+/// Clase que hace manejo de  sesión
+final public class BSURLSession: NSObject {
+    /// Tiempo limite de cada petición
+    internal let TIMEOUT = 10.0
+    /// Delegado de envio de infromación
+    private var delegate: BSConnectionDelegate?
+    /// Manegador de la sesión
+    private var manager: URLSession?
+    /// Inicializador
+    public override init() {
+        super.init()
+        self.manager = URLSession(configuration: loadConfiguration(), delegate: self, delegateQueue: nil)
+    }
+    /// Configura la sesión url
+    private func loadConfiguration() -> URLSessionConfiguration {
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForRequest  = TIMEOUT
+        return config
+    }
+    /// Función que inicia petición a un servicio web.
+    ///  - Parameters:
+    ///   - request: Data de servicio
+    ///   - requestName: Nombre del servicio
+    public func sendRequest(request: URLRequest, requestName: String) {
+        self.manager?.dataTask(with: request, completionHandler: { (data, response, error) in
+            if let responseServer = response as? HTTPURLResponse, let unwrapData = data {
+                switch BSResponeCode(rawValue: responseServer.statusCode) {
+                case .success:
+                    self.delegate?.recievedData(data: unwrapData, requestName: requestName)
+                case .failed(let code):
+                    if let error = error {
+                        self.delegate?.recievedError(error: error, code: code, requestName: requestName)
+                    }
+                }
+            }
+            
+        }).resume()
+    }
+}
+/// Extensión que contiene las funciones de delgado de sesión
+extension BSURLSession: URLSessionDelegate {}
