2090682e6c38c58176a28d9aaeaaeb44e56ed6eb
JosPerez <jos.perez.mtz@gmail.com>
2022-03-01 01:05:41 -0600

    Configuración inicial
    - Se inicializa el framework

---
+++ BackServices/BackServices/BackServices.h
@@ -0,0 +1,18 @@
+//
+//  BackServices.h
+//  BackServices
+//
+//  Created by jose perez on 01/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BackServices.
+FOUNDATION_EXPORT double BackServicesVersionNumber;
+
+//! Project version string for BackServices.
+FOUNDATION_EXPORT const unsigned char BackServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BackServices/PublicHeader.h>
+
+
