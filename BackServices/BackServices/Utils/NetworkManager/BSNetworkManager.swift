6db9334a1b1346ac355a5a6c306f96c83152e0a1
JosPerez <jos.perez.mtz@gmail.com>
2022-03-01 02:00:26 -0600

    Monitor de Red.
    - Se agrega clase con manejador de red

---
+++ BackServices/BackServices/Utils/NetworkManager/BSNetworkManager.swift
@@ -0,0 +1,69 @@
+//
+//  BSNetworkManager.swift
+//  BackServices
+//
+//  Created by jose perez on 01/03/22.
+//
+import Foundation
+import Network
+protocol BSNetworkManagerDelegate {
+    /// Función con el cambio de status en la red
+    func didNetworkChange(status: Bool)
+}
+final class BSNetworkManager {
+    /// Contiene el monitor de red.
+    static var shared: BSNetworkManager = BSNetworkManager()
+    ///
+    private var queue: DispatchQueue
+    /// Monitor de red.
+    private var cellMonitor: NWPathMonitor
+    /// Monitor de red.
+    private var wifiMonitor: NWPathMonitor
+    /// Contiene variable si es conectado a internet.
+    private var isWiFiAvailible: Bool
+    /// Contiene variable si es conectado a celular.
+    private var isCellAvailible: Bool
+    /// Delegado de cambio de red
+    var networkDelegate: BSNetworkManagerDelegate?
+    /// Iniicalizador
+    init() {
+        self.cellMonitor = NWPathMonitor(requiredInterfaceType: .cellular)
+        self.wifiMonitor = NWPathMonitor(requiredInterfaceType: .wifi)
+        self.queue = DispatchQueue.global(qos: .background)
+        self.isWiFiAvailible = false
+        self.isCellAvailible = false
+        self.wifiMonitor.start(queue: queue)
+        self.cellMonitor.start(queue: queue)
+    }
+    /// Iniciar monitoreo de red
+    func start() {
+        startWiFiMonitoring()
+        startCellMonitoring()
+    }
+    /// Revisar red de WiFi.
+    private func startWiFiMonitoring() {
+        cellMonitor.pathUpdateHandler = { path in
+            self.isCellAvailible = path.status == .satisfied
+            self.networkDelegate?.didNetworkChange(status: self.networkStatus())
+        }
+    }
+    /// Revisar red de Celuar.
+    private func startCellMonitoring() {
+        wifiMonitor.pathUpdateHandler = { path in
+            self.isWiFiAvailible = path.status == .satisfied
+            self.networkDelegate?.didNetworkChange(status: self.networkStatus())
+        }
+    }
+    /// Estatus de red.
+    /// - Returns: Valor si esta encendida.
+    func networkStatus() -> Bool {
+        return isCellAvailible || isWiFiAvailible
+    }
+    /// Termina monitoreo de red
+    func cancel() {
+        cellMonitor.cancel()
+        wifiMonitor.cancel()
+        self.isWiFiAvailible = false
+        self.isCellAvailible = false
+    }
+}
---
+++ BackServices/BackServices/WebServices/Base/BSBaseFacade.swift
@@ -0,0 +1,8 @@
+//
+//  BSBaseFacade.swift
+//  BackServices
+//
+//  Created by jose perez on 01/03/22.
+//
+import Foundation
+final class BSBaseFacade {}
